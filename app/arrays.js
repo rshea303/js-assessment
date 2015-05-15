99f9db8cad5fe70ab1c8a85e6f7b40454223936a
Rich Shea <richcshea@gmail.com>
2015-05-15 10:02:36 -0600

    pass more tests

--- app/arrays.js
+++ app/arrays.js
@@ -80,7 +80,12 @@ define(function() {
     },
 
     findAllOccurrences : function(arr, target) {
-
+      var result = [];                     
+      for (var i = 0; i < arr.length; i++) {
+        if (arr[i] === target)
+          result.push(i);
+      }
+      return result;
     }
   };
 });
