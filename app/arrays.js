63febdd07ad02b2a6327a4ec5e10d5467914652b
Rich Shea <richcshea@gmail.com>
2015-05-17 09:02:55 -0600

    one array test to go - duplicates

--- app/arrays.js
+++ app/arrays.js
@@ -24,9 +24,14 @@ define(function() {
     },
 
     removeWithoutCopy : function(arr, item) {
-        for (var i = 0; i < arr.length; i++) {
-          if (arr[i] === item) 
+        var i, len;
+
+        for (i = 0, len = arr.length; i < len; i++) {
+          if (arr[i] === item) { 
             arr.splice(i, 1);
+            i = i -1;
+            len = len -1;
+          };
         };
         return arr;
     },
