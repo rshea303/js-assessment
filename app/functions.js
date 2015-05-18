9fed68d797472dc4f851463430fd47d6cb64e5c2
Rich Shea <richcshea@gmail.com>
2015-05-18 07:41:31 -0600

    add to github

--- app/functions.js
+++ app/functions.js
@@ -3,15 +3,17 @@ if (typeof define !== 'function') { var define = require('amdefine')(module); }
 define(function() {
   return {
     argsAsArray : function(fn, arr) {
-
+      return fn.apply(null, arr);
     },
 
     speak : function(fn, obj) {
-
+      return fn.call(obj);
     },
 
     functionFunction : function(str) {
-
+      return function(arg) {
+        return str + ', ' + arg;
+      }
     },
 
     makeClosures : function(arr, fn) {
@@ -19,7 +21,9 @@ define(function() {
     },
 
     partial : function(fn, str1, str2) {
-
+      return function(str3) {
+        return fn.call(null, str1, str2, str3);
+      }
     },
 
     useArguments : function() {
